fb335e795e9d816bc5e935e8f560e11384e38b16
BLT Sandwich <bltsandwich@ULINUXV3.local>
2026-08-13 00:22:04 -0400

    add a bunch of shit

--- src/tty.c
+++ src/tty.c
@@ -1,3 +1,25 @@
+// Logic for console spawning and message broadcasting.
+
+// Copyright (C) 2026 BLT Sandwich
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.*
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// NOTE: AI was used in the writing of this file and its corresponding header file.
+// However, the author can assure you that this code has been thoroughly studied and tested by the author,
+// and has proven to be fully functional without major bugs.
+// Do note that further testing is encouraged for anybody who uses this program.
+
 #include "tty.h"
 #include <signal.h>
 #include <fcntl.h>
